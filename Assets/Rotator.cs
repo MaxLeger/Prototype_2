203a6fd6b1aea41e4c8c82447ecc4e13a37d03b9
MGL <maxleger1999@gmail.com>
2023-01-27 12:24:12 -0500

    Initial commit

---
+++ Assets/Rotator.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+ { 
+
+public float rotationSpeed = 0.3f;
+
+private bool rotate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.R))
+            {
+            rotate = true;
+        }
+        else
+        {
+            rotate = false;
+        }
+    }
+
+    // Update is called once per interval
+    void FixedUpdate()
+{
+    if (rotate)
+    {
+        this.GetComponent<Transform>().Rotate(0, 0.3f, 0);
+    }
+}
+
+}
+
+    
+ 
